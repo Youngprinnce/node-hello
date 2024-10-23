f1d01c8d38f38490f6f814a24e82465e33cde2e9
youngprinnce <ajiboyeadedotun16@gmail.com>
2024-10-23 22:19:19 +0100

    Update .gitignore and add .DS_Store and node_modules Add .env file with database and Redis configurations Add Dockerfile for containerization Add .dockerignore to exclude unnecessary files from Docker build Update package.json with dependencies Remove .vscode/settings.json Update index.js with Express, Sequelize, MySQL, and Redis configurations

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+require('dotenv').config();
+const express = require('express');
+const { Sequelize } = require('sequelize');
+const Redis = require('ioredis'); // Import ioredis
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+const app = express();
+const port = process.env.APP_PORT || 3000;
+
+// Connect to MySQL
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT
+  }
+);
+
+// Connect to Redis
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  db: process.env.REDIS_DB
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Test database connection
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err));
+
+// Test Redis connection
+redis.ping()
+  .then(() => console.log('Connected to Redis...'))
+  .catch(err => console.log('Redis Error: ' + err));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
